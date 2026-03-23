62d677b46976b0b86e56fc2b0fcea0b69a19b397
The Code Bro <twittercodebro@gmail.com>
2026-03-23 09:37:21 +0100

    made nav menu responsive for mobile devices and added new icons

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,27 +1,57 @@
+//Save the DOM elements needed into a variable
 const header = document.querySelector("header");
-// const menuBtn = document.querySelector(".menu-btn");
+const contactBtn = document.querySelector(".contact-link");
 const menuIcon = document.querySelector(".menu-btn img");
 const menuPanel = document.querySelector("nav ul");
-const navLinks = document.querySelectorAll("nav ul li");
 
-// console.log(menuBtn, menuIcon, menuPanel);
-
-const currentPage = window.location.href;
-
-console.log(currentPage, navLinks);
-
-header.addEventListener("click", (e) => {
-  //menu toggle
-  if (e.target.matches(".menu-btn img")) {
-    menuPanel.classList.toggle("is-toggled");
-  }
-  if (menuPanel.classList.contains("is-toggled")) {
-    header.classList.add("is-toggled-active");
-    menuIcon.src = "./icons/close-icon.svg";
-  } else {
-    header.classList.remove("is-toggled-active");
-    menuIcon.src = "./icons/menu-icon.svg";
-  }
-
-  //Preventing page reload on current page
+//Append contact button as a navlink child on mobile devices
+window.addEventListener("resize", () => {
+    appendContactBtn();
 });
+
+function appendContactBtn() {
+    if (window.innerWidth <= "768") {
+        menuPanel.append(contactBtn);
+        document.querySelector(".contact-link img").src =
+            "./icons/arrow-purple.svg";
+    } else {
+        document.querySelector(".contact-link img").src = "./icons/left-arrow.svg";
+    }
+}
+appendContactBtn();
+
+//A delegated event listener for menu button and navlinks
+header.addEventListener("click", (e) => {
+    //menu toggle
+    if (e.target.matches(".menu-btn img")) {
+        if (!menuPanel.classList.contains("is-toggled")) {
+            openMenu(menuPanel, header, menuIcon);
+        } else {
+            closeMenu(menuPanel, header, menuIcon);
+        }
+    }
+
+    //Preventing page reload when current page navlink is clicked
+    if (e.target.closest("a.nav-link")) {
+        const navLink = e.target.closest("a.nav-link");
+        const navLinkPath = new URL(navLink).pathname;
+        const currentPagePath = window.location.pathname;
+
+        if (navLinkPath === currentPagePath) {
+            closeMenu(menuPanel, header, menuIcon);
+            e.preventDefault();
+        }
+    }
+});
+
+function openMenu(menu, parent, icon) {
+    menu.classList.add("is-toggled");
+    parent.classList.add("is-toggled-active");
+    icon.src = "./icons/close-icon.svg";
+}
+
+function closeMenu(menu, parent, icon) {
+    menu.classList.remove("is-toggled");
+    parent.classList.remove("is-toggled-active");
+    icon.src = "./icons/menu-icon.svg";
+}
