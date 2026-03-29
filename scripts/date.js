29d06daacc0c9dcb1c6b96e08c35e39631b088bf
The Code Bro <twittercodebro@gmail.com>
2026-03-29 19:22:00 +0100

    completed home page and made it responsive

---
+++ scripts/date.js
@@ -0,0 +1,6 @@
+const copyrightYearElement = document.querySelector(".copyright h3");
+
+const date = new Date();
+const currentYear = date.getFullYear();
+
+copyrightYearElement.textContent = `© ${currentYear} Waves. All rights reserved.`;
--- scripts/menu.js
+++ scripts/menu.js
@@ -2,56 +2,56 @@
 const header = document.querySelector("header");
 const contactBtn = document.querySelector(".contact-link");
 const menuIcon = document.querySelector(".menu-btn img");
-const menuPanel = document.querySelector("nav ul");
+const menuPanel = document.querySelector("header nav ul");
 
 //Append contact button as a navlink child on mobile devices
 window.addEventListener("resize", () => {
-  appendContactBtn();
+    appendContactBtn();
 });
 
 function appendContactBtn() {
-  if (window.innerWidth <= "768") {
-    menuPanel.append(contactBtn);
-    document.querySelector(".contact-link img").src =
-      "./icons/arrow-purple.svg";
-  } else {
-    document.querySelector(".contact-link img").src = "./icons/left-arrow.svg";
-  }
+    if (window.innerWidth <= "768") {
+        menuPanel.append(contactBtn);
+        document.querySelector(".contact-link img").src =
+            "./icons/arrow-purple.svg";
+    } else {
+        document.querySelector(".contact-link img").src = "./icons/left-arrow.svg";
+    }
 }
 appendContactBtn();
 
 //A delegated event listener for menu button and navlinks
 header.addEventListener("click", (e) => {
-  //menu toggle
-  if (e.target.matches(".menu-btn img")) {
-    if (!menuPanel.classList.contains("is-toggled")) {
-      openMenu(menuPanel, header, menuIcon);
-    } else {
-      closeMenu(menuPanel, header, menuIcon);
+    //menu toggle
+    if (e.target.matches(".menu-btn img")) {
+        if (!menuPanel.classList.contains("is-toggled")) {
+            openMenu(menuPanel, header, menuIcon);
+        } else {
+            closeMenu(menuPanel, header, menuIcon);
+        }
     }
-  }
 
-  //Preventing page reload when current page navlink is clicked
-  if (e.target.closest("a.nav-link")) {
-    const navLink = e.target.closest("a.nav-link");
-    const navLinkPath = new URL(navLink).pathname;
-    const currentPagePath = window.location.pathname;
+    //Preventing page reload when current page navlink is clicked
+    if (e.target.closest("a.nav-link")) {
+        const navLink = e.target.closest("a.nav-link");
+        const navLinkPath = new URL(navLink).pathname;
+        const currentPagePath = window.location.pathname;
 
-    if (navLinkPath === currentPagePath) {
-      closeMenu(menuPanel, header, menuIcon);
-      e.preventDefault();
+        if (navLinkPath === currentPagePath) {
+            closeMenu(menuPanel, header, menuIcon);
+            e.preventDefault();
+        }
     }
-  }
 });
 
 function openMenu(menu, parent, icon) {
-  menu.classList.add("is-toggled");
-  parent.style.position = "fixed";
-  icon.src = "./icons/close-icon.svg";
+    menu.classList.add("is-toggled");
+    // parent.style.position = "fixed";
+    icon.src = "./icons/close-icon.svg";
 }
 
 function closeMenu(menu, parent, icon) {
-  menu.classList.remove("is-toggled");
-  parent.style.position = "static";
-  icon.src = "./icons/menu-icon.svg";
+    menu.classList.remove("is-toggled");
+    // parent.style.position = "static";
+    icon.src = "./icons/menu-icon.svg";
 }
