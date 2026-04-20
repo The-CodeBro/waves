59a65389fce177a35c6213176a0fcce6fb5360d4
The Code Bro <twittercodebro@gmail.com>
2026-04-20 18:52:38 +0100

    completed form functionality and UI

--- scripts/formValidator.js
+++ scripts/formValidator.js
@@ -4,106 +4,165 @@ const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const projectInput = document.querySelector("#project");
 const indicators = document.querySelectorAll(".indicator");
+const fileUpload = document.getElementById("file-upload");
+const fileErrorMessage = document.querySelector(".file-error");
+const showFile = document.querySelector(".show-file");
 const errorMessage = document.querySelector(".error-message");
 
-const userRequests = [];
-
 form.addEventListener("keyup", (e) => {
-  const name = nameInput.value.trim();
-  const email = emailInput.value.trim();
-  const projectDetail = projectInput.value.trim();
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    const projectDetail = projectInput.value.trim();
 
-  if (e.target.dataset.input === "name") {
-    validateName(name);
-  }
+    if (e.target.dataset.input === "name") {
+        validateName(name);
+    }
 
-  if (e.target.dataset.input === "email") {
-    validateMail(email);
-  }
+    if (e.target.dataset.input === "email") {
+        validateMail(email);
+    }
 
-  if (e.target.dataset.input === "project-detail") {
-    validateProjectDetail(projectDetail);
-  }
+    if (e.target.dataset.input === "project-detail") {
+        validateProjectDetail(projectDetail);
+    }
+});
+
+fileUpload.addEventListener("change", (e) => {
+    const file = fileUpload.files[0];
+
+    handleFileValidation(file);
 });
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  let isValid = true;
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    const projectDetail = projectInput.value.trim();
+    const file = fileUpload.files[0];
 
-  const name = nameInput.value.trim();
-  const email = emailInput.value.trim();
-  const projectDetail = projectInput.value.trim();
-
-  if (
-    !validateName(name) ||
-    !validateMail(email) ||
-    !validateProjectDetail(projectDetail)
-  ) {
-    isValid = false;
-    errorMessage.innerHTML = "<p>Please fix error and resend request</p>";
-    console.log("submit prevented");
-    return;
-  }
-
-  if (isValid) {
-    form.submit();
+    fileErrorMessage.innerHTML = "";
     errorMessage.innerHTML = "";
-    console.log("submitted");
-  }
+
+    const fileIsValid = handleFileValidation(file);
+
+    if (!validateName(name) ||
+        !validateMail(email) ||
+        !validateProjectDetail(projectDetail) ||
+        !fileIsValid
+    ) {
+        errorMessage.innerHTML = "<p>Please fix error and resend request</p>";
+
+        return;
+    }
+
+    // if (!fileIsValid){
+
+    // } return;
+
+    form.submit();
 });
 
 function validateName(name) {
-  const nameRegex = /^[a-zA-Z\s'-]{3,50}$/;
+    const nameRegex = /^[a-zA-Z\s'-]{3,50}$/;
 
-  let nameValidity;
-  const indicatorIndex = indicators[0];
+    const indicatorIndex = indicators[0];
 
-  if (nameRegex.test(name)) {
-    nameValidity = true;
-    indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-  } else if (name === "") {
-    indicatorIndex.innerHTML = "";
-  } else {
-    nameValidity = false;
-    indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
-  }
-
-  return nameValidity;
+    if (nameRegex.test(name)) {
+        indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+        return true;
+    } else if (name === "") {
+        indicatorIndex.innerHTML = "";
+        return false;
+    } else {
+        indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
+        return false;
+    }
 }
 
 function validateMail(mail) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  let emailValidity;
-  const indicatorIndex = indicators[1];
+    const indicatorIndex = indicators[1];
 
-  if (emailRegex.test(mail)) {
-    emailValidity = true;
-    indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-  } else if (mail === "") {
-    indicatorIndex.innerHTML = "";
-  } else {
-    emailValidity = false;
-    indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
-  }
-
-  return emailValidity;
+    if (emailRegex.test(mail)) {
+        indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+        return true;
+    } else if (mail === "") {
+        indicatorIndex.innerHTML = "";
+        return false;
+    } else {
+        indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
+        return false;
+    }
 }
 
 function validateProjectDetail(projectDetail) {
-  let detailValidity;
-  const indicatorIndex = indicators[2];
+    const indicatorIndex = indicators[2];
 
-  if (projectDetail.length > 10) {
-    detailValidity = true;
-    indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-  } else if (projectDetail === "") {
-    indicatorIndex.innerHTML = "";
-  } else {
-    detailValidity = false;
-    indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
-  }
+    if (projectDetail.length > 10) {
+        indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+        return true;
+    } else if (projectDetail === "") {
+        indicatorIndex.innerHTML = "";
+        return false;
+    } else {
+        indicatorIndex.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
+        return false;
+    }
+}
 
-  return detailValidity;
+function handleFileValidation(file) {
+    if (!file) {
+        fileErrorMessage.innerHTML = "";
+        showFile.innerHTML = "";
+        return true;
+    }
+
+    const result = validateFile(file);
+
+    if (!result.valid) {
+        fileErrorMessage.innerHTML = result.errorMessage;
+        showFile.innerHTML = "";
+        return false;
+    }
+
+    fileErrorMessage.innerHTML = "";
+    showFile.innerHTML = `
+        <p>${file.name}</p>
+        <i class="fa-solid fa-circle-check"></i>
+        `;
+    return true;
+}
+
+function validateFile(file) {
+    const acceptedTypes = [
+        "image/png",
+        "image/jpeg",
+        "image/jpg",
+        "image/webp",
+        "application/pdf",
+        "application/msword", // .doc
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document", // .docx
+    ];
+
+    const maxFileSize = 5 * 1024 * 1024;
+
+    if (!acceptedTypes.includes(file.type)) {
+        return {
+            valid: false,
+            errorMessage: "<p>File can only be IMAGE, PDF or DOC</p>",
+        };
+    }
+
+    if (file.size > maxFileSize) {
+        return {
+            valid: false,
+            errorMessage: "<p>File is too big, max(5mb)</p>",
+        };
+    }
+
+    return {
+        valid: true,
+    };
 }
